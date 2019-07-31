51cb25a8606fc4a02848f31a059b3422fd136676
keenanhammonds <keenan.hammonds@gmail.com>
2019-07-31 10:04:07 -0400

    added api key and url

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const parser = require('body-parser');
 const app = express();
+const axios = require('axios');
+const url = 'https://api.musixmatch.com/ws/1.1/'
+const apiKey = 'd6e7290a1d7028ba94f734e4a5d7e687';
+
+parser.json();
+
+axios.get
 
 app.listen(4000, () => console.log('server up and running on port 3000!'));
