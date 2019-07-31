086c3faf9d3991e623a2600f6886aeab6e1d5c43
keenanhammonds <keenan.hammonds@gmail.com>
2019-07-31 12:10:59 -0400

    back to the drawing board

--- index.js
+++ index.js
@@ -2,25 +2,9 @@ const express = require('express');
 const parser = require('body-parser');
 const app = express();
 const axios = require('axios');
-const url = 'https://api.deezer.com/'
-const album = '/Album'
+
 
 parser.json();
-
-
-
-axios.get(url, album)
-  .then(function (res) {
-    // handle success
-    console.log(res)
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .finally(function () {
-    // always executed
-  });
 
 
 app.listen(4000, () => console.log('server up and running on port 3000!'));
