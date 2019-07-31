bac77e743a1add0fc8a88aefe17f7311921ff7be
keenanhammonds <keenan.hammonds@gmail.com>
2019-07-31 10:37:29 -0400

    using new api for data

--- index.js
+++ index.js
@@ -2,11 +2,24 @@ const express = require('express');
 const parser = require('body-parser');
 const app = express();
 const axios = require('axios');
-const url = 'https://api.musixmatch.com/ws/1.1/'
-const apiKey = 'd6e7290a1d7028ba94f734e4a5d7e687';
+const url = 'https://api.deezer.com/version/service/id/method/?parameters'
 
 parser.json();
 
-axios.get
+
+
+axios.get(url, apiKey)
+  .then(function (res) {
+    // handle success
+    console.log(res;
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+  });
+
 
 app.listen(4000, () => console.log('server up and running on port 3000!'));
